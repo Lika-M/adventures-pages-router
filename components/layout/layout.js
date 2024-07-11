7ce9092cef207e1d0b5d21e4178414f54d7f01ad
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-07-11 20:26:40 +0300

    Added global and layout styling.

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,11 @@
 import MainNavigation from "./main-navigation.js";
+import classes from './layout.module.css';
 
 export default function Layout({ children }) {
     return (
         <>
             <MainNavigation />
-            <main>{children}</main>
+            <main className={classes.main}>{children}</main>
         </>
     );
 }
