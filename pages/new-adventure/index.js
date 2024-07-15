0a14ffbe8d583cd2caad95b377ea7227aa9bc59c
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-07-15 12:50:10 +0300

    Added redirect to home page after successfully submitting user data.

--- pages/new-adventure/index.js
+++ pages/new-adventure/index.js
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router.js";
+
 import NewAdventureForm from "@/components/adventures/new-adventure-form.js";
 import { createAdventure } from "@/db-lib/util.js";
 
 export default function NewAdventure() {
+  const router = useRouter();
 
   function addHandler(data) {
     createAdventure(data);
+    router.push('/')
   }
   
   return (
