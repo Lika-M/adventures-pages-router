4210cd6e595946eea9e5a05733e8b5d2b32874fe
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-07-15 17:14:49 +0300

    Retrieve adventure details using getStaticProps and getStaticPaths from MongoDB with loading/error states.

--- pages/[adventureId]/index.js
+++ pages/[adventureId]/index.js
@@ -1,35 +1,116 @@
-import AdventureDetail from "@/components/adventures/adventure-detail.js";
-import { DUMMY_DATA } from "../index.js";
+import { ObjectId } from 'mongodb';
 
-export default function Details({adventure}) {
-    return <AdventureDetail {...adventure} />
+import AdventureDetail from "@/components/adventures/adventure-detail.js";
+import { connectToDB } from "@/db-lib/db.js";
+
+export default function Details({ adventure, loading, error }) {
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Adventure not found!</p>;
+    }
+
+    return <AdventureDetail {...adventure} />;
 }
 
 export async function getStaticProps(context) {
     const adventureId = context.params.adventureId;
 
-    console.log(adventureId);
-    const adventure = DUMMY_DATA.find(adv => adv.id === adventureId);
+    let client;
+    let adventure = {};
+    let loading = true;
+    let error = null;
+
+    try {
+        client = await connectToDB();
+    } catch (err) {
+        return {
+            props: {
+                adventure,
+                loading: false,
+                error: { message: 'Could not connect to database.' }
+            },
+            revalidate: 3600,
+        };
+    }
+
+    try {
+        const db = client.db('adventures');
+        const collection = db.collection('destinations');
+        const objectId =  ObjectId.createFromHexString(adventureId);
+        adventure = await collection.findOne({ _id: objectId });
+
+        if (!adventure) {
+            throw new Error('Adventure not found');
+        }
+
+        adventure = {
+            id: adventure._id.toString(),
+            title: adventure.title,
+            image: adventure.image,
+            address: adventure.address,
+            description: adventure.description
+        }
+
+        loading = false;
+
+    } catch (err) {
+        error = { message: 'Could not fetch adventure data.' };
+    } finally {
+        if (client) {
+            client.close();
+        }
+    }
 
     return {
         props: {
-            adventure
+            adventure,
+            loading,
+            error
         },
         revalidate: 300
     }
 }
 
 export async function getStaticPaths() {
+    let client;
+    let adventures = [];
+
+    try {
+        client = await connectToDB();
+    } catch (error) {
+        return {
+            fallback: false,
+            paths: [],
+        };
+    }
+
+    try {
+        const db = client.db('adventures');
+        const collection = db.collection('destinations');
+        adventures = await collection.find({}, { _id: 1 }).toArray();
+
+    } catch (error) {
+        return {
+            fallback: false,
+            paths: []
+        }
+    } finally {
+        if (client) {
+            client.close();
+        }
+    }
+
+    const paths = adventures.map(adventure => ({
+        params: {
+            adventureId: adventure._id.toString()
+        }
+    }))
 
     return {
         fallback: false,
-        paths: [
-            {
-                params: {adventureId: 'ad_1'}
-            },
-            {
-                params: {adventureId: 'ad_2'}
-            }
-        ]
+        paths
     }
 }
