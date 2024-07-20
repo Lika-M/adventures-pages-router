12b44983ac282e29c4d8a34e6f2d5a6807b86370
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-07-20 17:17:47 +0300

    Refactor client-side authentication form - Used useState for form input management - Added loading state for improved user feedback during form submission like button disabled.

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,31 +1,61 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router.js';
 
 import { createUser } from '@/db-lib/util.js';
 import classes from './auth-form.module.css';
 
 function AuthForm() {
     const [isLogin, setIsLogin] = useState(true);
-
-    const inputEmail = useRef();
-    const inputPassword = useRef();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState({ email: '', password: '' });
+    const router = useRouter();
 
     async function signHandler(event) {
         event.preventDefault();
 
+        // TODO validation
+        if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            setErrorMessage(state => ({
+                ...state,
+                email: 'Enter a valid email address'
+            }));
+        }
+        
+        if (!password || password.trim().length < 6) {
+            setErrorMessage(state => ({
+                ...state,
+                password: 'Password must be at least 6 characters long'
+            }));
+        }
+
+        if (errorMessage.some(x => x === '')) {
+            return;
+        }
+
         const user = {
-            email: inputEmail.current.value,
-            password: inputPassword.current.value,
+            email,
+            password,
             createdAt: new Date().toISOString()
         }
 
-        if (!isLogin) {
-            try {
-                const result = await createUser(user);
-            } catch (error) {
-                alert(error.message);
+        try {
+            if (isLogin) {
+                console.log('login');
+                // check user credentials
+                // router.replace('/adventures');
+            } else {
+                await createUser(user);
+                router.replace('/adventures');
             }
-        } else {
-            console.log('login')
+        } catch (error) {
+            //TODO add 404 page
+            // router.push('/error)
+        } finally {
+            setIsLoading(false);
+            setEmail('');
+            setPassword('');
         }
     }
 
@@ -39,18 +69,35 @@ function AuthForm() {
             <form onSubmit={signHandler}>
                 <div className={classes.control}>
                     <label htmlFor='email'>Enter your email</label>
-                    <input type='email' id='email' required ref={inputEmail} />
+                    <input
+                        type='email'
+                        id='email'
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor='password'>Enter your password</label>
-                    <input type='password' id='password' required ref={inputPassword} />
+                    <input
+                        type='password'
+                        id='password'
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div className={classes.actions}>
-                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                    <button type='submit' disabled={isLoading}>
+                        {isLogin ? 'Login' : 'Create Account'}
+                    </button>
                 </div>
                 <div className={classes.toggle}>
-                    <span>{isLogin ? 'Not have an account?' : 'Already have an account?'}</span>
-                    <span onClick={toggleAction}> {isLogin ? 'Register here.' : 'Login here.'}
+                    <span>
+                        {isLogin ? 'Not have an account?' : 'Already have an account?'}
+                    </span>
+                    <span onClick={toggleAction}>
+                        {isLogin ? 'Register here.' : 'Login here.'}
                     </span>
                 </div>
             </form>
