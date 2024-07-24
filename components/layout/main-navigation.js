cfb2a54abd8c6043aca1717fddf1965cfd9471ab
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-07-24 18:47:19 +0300

    Implemented user logout functionality and updated main navigation to display different content for unauthenticated users.

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,15 +1,23 @@
 import Link from 'next/link';
 import { useState } from 'react';
-
+import { useSession, signOut } from 'next-auth/react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import classes from './main-navigation.module.css';
 
 export default function MainNavigation() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const { data: session, status } = useSession();
+    console.log(status);  // loading | authenticated | unauthenticated
+
+    const isAuth = status === 'authenticated';
 
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!mobileMenuOpen);
     };
+
+    async function logoutHandler() {
+        await signOut();
+    }
 
     return (
         <header className={classes.header}>
@@ -18,22 +26,29 @@ export default function MainNavigation() {
                     Adventures
                 </Link>
             </div>
-            <nav className={mobileMenuOpen ? classes['open-nav'] : classes['close-nav']}>
+            <nav className={`${classes.nav} ${mobileMenuOpen ? classes['open-nav'] : classes['close-nav']}`}>
                 <div className={classes.menuToggle} onClick={toggleMobileMenu}>
                     <div className={classes.toggleIcon}>
-                        {!mobileMenuOpen ? <FaBars/> : <FaTimes/> }
+                        {!mobileMenuOpen ? <FaBars /> : <FaTimes />}
                     </div>
                 </div>
                 <ul>
+                    {isAuth &&
+                        <li className={classes['new-adventure']}>
+                            <Link href='/new-adventure'>Add Journey</Link>
+                        </li>
+                    }
                     <li>
                         <Link href='/adventures'>All Impressive Places</Link>
                     </li>
-                    <li>
-                        <Link href='/new-adventure'>Add Journey</Link>
-                    </li>
-                    <li>
-                        <Link href='/auth'>Login</Link>
-                    </li>
+                    {!isAuth 
+                        ? <li className={classes['auth-link']}>
+                            <Link href='/auth'>Login</Link>
+                        </li>
+                        : <li className={classes['auth-link']}>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    }
                 </ul>
             </nav>
         </header>
